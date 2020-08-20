4d9522adbaef80f01d24e14d1b7c5be6ab96d2e8
Eli Musgrove <eli.musgrove9@gmail.com>
2020-08-20 18:39:24 -0500

    Add isOver() method.

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -16,6 +16,33 @@ class Board {
         Arrays.fill(boardItems, 'E');
     }
 
+    public int isOver() {
+        // Check rows
+        for (int i=0; i < 9; i+=3) {
+            if (boardItems[i] != 'e' && boardItems[i] == boardItems[i + 1] && boardItems[i + 1] == boardItems[i + 2]) {
+                return boardItems[i] == 'X' ? 1 : 2;
+            }
+        }
+
+        // Check columns
+        for (int i=0; i < 3; i++) {
+            if (boardItems[i] != 'e' && boardItems[i] == boardItems[i + 3] && boardItems[i + 3] == boardItems[i + 6]) {
+                return boardItems[i] == 'X' ? 1 : 2;
+            }
+        }
+
+        // Check diagonals
+        if (boardItems[0] != 'e' && boardItems[0] == boardItems[4] && boardItems[4] == boardItems[8]) {
+            return boardItems[0] == 'X' ? 1 : 2;
+        }
+        if (boardItems[2] != 'e' && boardItems[2] == boardItems[4] && boardItems[4] == boardItems[6]) {
+            return boardItems[0] == 'X' ? 1 : 2;
+        }
+
+        // Game not over yet
+        return 0;
+    }
+
     @Override
     public String toString() {
         String returnString = "";
